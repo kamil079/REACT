d2c0a9c2787212eaa767d51af7f9caa92b0a708e
kamil079 <kamil.szczepaniec@wp.pl>
2022-04-04 22:25:05 +0200

    cd

---
+++ 01_Dzien_1/02_Stan_aplikacji/03_Zadanie_3/js/RandomNumbers.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class RandomNumbers extends Component {
+    state = {
+        arr: [],
+    };
+
+    randomNr = () => {
+        this.setState((prevState) => {
+            return {
+                arr:
+                    prevState.arr + Math.floor(Math.random() * (999 - 100 + 1)),
+            };
+        });
+    };
+
+    render() {
+        const { arr } = this.state;
+        return (
+            <>
+                <ul>
+                    {arr.forEach((el, i) => {
+                        return <li key={i}>{el}</li>;
+                    })}
+                </ul>
+                <button onClick={this.randomNr}>random nr</button>
+            </>
+        );
+    }
+}
+
+export default function App() {
+    return <RandomNumbers />;
+}
