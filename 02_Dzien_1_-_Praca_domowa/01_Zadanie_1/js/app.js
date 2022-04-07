cb6d6b09c74376173dc4a6174b8399723e23d1f9
kamil079 <kamil.szczepaniec@wp.pl>
2022-04-07 20:17:13 +0200

    class and function comp

--- 02_Dzien_1_-_Praca_domowa/01_Zadanie_1/js/app.js
+++ 02_Dzien_1_-_Praca_domowa/01_Zadanie_1/js/app.js
@@ -1,12 +1,23 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 function UpperCaseText(props) {
-  return <h2>{props.text.toLocaleUpperCase()}</h2>;
+    return <h2>{props.text.toLocaleUpperCase()}</h2>;
 }
 
+class UpperCaseText2 extends Component {
+    render() {
+        return <h2>{this.props.text.toLocaleUpperCase()}</h2>;
+    }
+}
+
+let text = 'hello'
 
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
+    <>
+        <h1>Hello, World!</h1>
+        <UpperCaseText text={text}/>
+        <UpperCaseText2 text={text}/>
+    </>,
+    document.getElementById("app")
 );
