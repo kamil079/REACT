7cc2437dcff7b1bc84e06a4984d053385c02953b
kamil079 <kamil.szczepaniec@wp.pl>
2022-04-11 22:00:34 +0200

    with internet help..

--- 03_Dzien_2/02_useEffect/05_Zadanie_5/js/Stopwatch.js
+++ 03_Dzien_2/02_useEffect/05_Zadanie_5/js/Stopwatch.js
@@ -2,20 +2,48 @@ import React, { useState, useEffect } from "react";
 
 const StopWatch = () => {
     const [time, setTime] = useState(0);
-    // const [timeOn, setTimeOn] = useState(false);
-    const [intervalID, setInt] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
     const [laps, setLaps] = useState([]);
+
+    function toggle() {
+        setIsRunning(!isRunning);
+    }
+
+    function reset() {
+        setTime(0);
+        setIsRunning(false);
+    }
+
+    function laping() {
+        setLaps([...laps, time]);
+            return laps
+    }
+
+    useEffect(() => {
+        let intervalId = null;
+        if (isRunning) {
+            intervalId = setInterval(() => {
+                setTime((time) => time + 1);
+            }, 1000);
+            console.log(time);
+        } else if (!isRunning) {
+            clearInterval(intervalId);
+        }
+        return () => clearInterval(intervalId);
+    }, [isRunning, time]);
 
     return (
         <>
             <h1>Time: {time}s</h1>
-            <button>Lap</button>
-            <button>Stop</button>
-            <button>Reset</button>
-            <button>Start</button>
+            <button onClick={laping}>Lap</button>
+            <button onClick={toggle}>Stop</button>
+            <button onClick={reset}>Reset</button>
+            <button onClick={toggle}>Start</button>
             <ul>
                 Laps:
-                {}
+                {laps.map((el,i) => {
+                    return <li key={i}>{el}</li>
+                })}
             </ul>
         </>
     );
