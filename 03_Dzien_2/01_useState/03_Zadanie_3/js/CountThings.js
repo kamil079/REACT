47c66262ecffdf2037f31a8983b11929457a77be
kamil079 <kamil.szczepaniec@wp.pl>
2022-06-14 21:19:17 +0200

    uperlower txt

--- 03_Dzien_2/01_useState/03_Zadanie_3/js/CountThings.js
+++ 03_Dzien_2/01_useState/03_Zadanie_3/js/CountThings.js
@@ -1,28 +1,56 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 
 const CountThings = () => {
-    const [bank, setBank] = useState(10000);
+    const [account, setAccount] = useState(10000);
     const [age, setAge] = useState(28);
 
-    const richer = () => {
-        setBank(prevState => prevState + 100000000)
+    const getRich = () => {
+        setAccount(prevState => prevState + 1000)
     }
 
-    const older = () => {
+    const getOld = () => {
         setAge(prevState => prevState + 1)
     }
 
-
     return (
-        <div>
-            <p>💰 so much money: ${bank}</p>
-            <p>Age: {age}</p>
-            <button onClick={richer}>get rich!!!</button>
-            <button onClick={older}>get older!!!</button>
-        </div>
+        <>
+            <div>
+                <p>💰 so much money: ${account}</p>
+                <button onClick={getRich}>Get rich!</button>
+                <p>age: {age}</p>
+                <button onClick={getOld}>Get old 😕</button>
+            </div>
+        </>
     )
-};
+}
 
-export default () => {
-    return <CountThings />;
-};
+export default CountThings;
+
+// import React, { useState } from "react";
+
+// const CountThings = () => {
+//     const [bank, setBank] = useState(10000);
+//     const [age, setAge] = useState(28);
+
+//     const richer = () => {
+//         setBank(prevState => prevState + 100000000)
+//     }
+
+//     const older = () => {
+//         setAge(prevState => prevState + 1)
+//     }
+
+
+//     return (
+//         <div>
+//             <p>💰 so much money: ${bank}</p>
+//             <p>Age: {age}</p>
+//             <button onClick={richer}>get rich!!!</button>
+//             <button onClick={older}>get older!!!</button>
+//         </div>
+//     )
+// };
+
+// export default () => {
+//     return <CountThings />;
+// };
