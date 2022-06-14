9126f59eb377c5daa52254e18b22fe99519592c6
kamil079 <kamil.szczepaniec@wp.pl>
2022-06-14 21:52:47 +0200

    toDoList useStaet

--- 03_Dzien_2/01_useState/06_Zadanie_6/js/ToDoList.js
+++ 03_Dzien_2/01_useState/06_Zadanie_6/js/ToDoList.js
@@ -1,24 +1,47 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const ToDoList = () => {
-    const [tasks, setArr] = useState([]);
+    const [arr, setArr] = useState([])
 
-    const addingTask = () => {
-        setArr((prevState) => [...prevState, 1]);
-    };
+    const addToList = () => {
+        setArr([...arr, ''])
+    }
 
-    return (
+    return(
         <>
+            <button onClick={addToList}>Add Task!</button>
             <ul>
-                {tasks.map((nr,i) => {
-                    return <li key={i}>zadanie nr{i + 1}</li>;
+                {arr.map((el,i) => {
+                    return <li key={i}>{el} task {i + 1}</li>//el jest nie potrzebne?
                 })}
             </ul>
-            <button onClick={addingTask}>add task</button>
         </>
-    );
-};
+    )
+}
 
-export default () => {
-    return <ToDoList />;
-};
+export default ToDoList;
+
+// import React, { useState } from "react";
+
+// const ToDoList = () => {
+//     const [tasks, setArr] = useState([]);
+
+//     const addingTask = () => {
+//         setArr((prevState) => [...prevState, 1]);
+//     };
+
+//     return (
+//         <>
+//             <ul>
+//                 {tasks.map((nr,i) => {
+//                     return <li key={i}>zadanie nr{i + 1}</li>;
+//                 })}
+//             </ul>
+//             <button onClick={addingTask}>add task</button>
+//         </>
+//     );
+// };
+
+// export default () => {
+//     return <ToDoList />;
+// };
