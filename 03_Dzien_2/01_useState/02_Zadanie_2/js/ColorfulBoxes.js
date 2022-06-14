3b2f395968fe306c2ac61e62c3ba9cd60c06ef00
kamil079 <kamil.szczepaniec@wp.pl>
2022-06-14 15:24:09 +0200

    add box onClick with randomColor

--- 03_Dzien_2/01_useState/02_Zadanie_2/js/ColorfulBoxes.js
+++ 03_Dzien_2/01_useState/02_Zadanie_2/js/ColorfulBoxes.js
@@ -1,30 +1,68 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 
 const ColorfulBoxes = () => {
-    const [arr, setArr] = useState([]);
+    const [arr, setArr] = useState([])
 
     let style = {
-        height: "100px",
-        width: "100px",
-    };
+        height: '200px',
+        width: '200px'
+    }
 
-    const handleClick = () => {
-        setArr([...arr, '#' + Math.floor(Math.random() * 16777215).toString(16)]);
-    };
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+    const addBox = () => {
+        setArr([...arr,  "#" + randomColor])
+    }
 
     return (
         <>
-            <button onClick={handleClick}>Dodaj boxa!</button>
-            <div>
-                {console.log(arr)}
-                {arr.map((el, i) => {
-                    return <div key={i} style={{ ...style, backgroundColor: el }}></div>;
+            <button onClick={addBox}>Add box!</button>
+            <div style={{display: 'flex'}}>
+                {arr.map((el,i) =>{
+                    return <div key={i} style={{...style, background: el}}></div>
                 })}
             </div>
         </>
-    );
-};
+    )
+}
 
-export default () => {
-    return <ColorfulBoxes />;
-};
+export default ColorfulBoxes;
+
+// import React, { useState } from "react";
+
+// const ColorfulBoxes = () => {
+//     const [arr, setArr] = useState([]);
+
+//     let style = {
+//         height: "100px",
+//         width: "100px",
+//     };
+
+//     const handleClick = () => {
+//         setArr([
+//             ...arr,
+//             "#" + Math.floor(Math.random() * 16777215).toString(16),
+//         ]);
+//     };
+
+//     return (
+//         <>
+//             <button onClick={handleClick}>Dodaj boxa!</button>
+//             <div>
+//                 {console.log(arr)}
+//                 {arr.map((el, i) => {
+//                     return (
+//                         <div
+//                             key={i}
+//                             style={{ ...style, backgroundColor: el }}
+//                         ></div>
+//                     );
+//                 })}
+//             </div>
+//         </>
+//     );
+// };
+
+// export default () => {
+//     return <ColorfulBoxes />;
+// };
