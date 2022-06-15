036d6bfff73ff96f5614d9612abd3ffceee84f97
kamil079 <kamil.szczepaniec@wp.pl>
2022-06-15 10:10:41 +0200

    counter in title

--- 03_Dzien_2/02_useEffect/02_Zadanie_2/js/CustomTitle.js
+++ 03_Dzien_2/02_useEffect/02_Zadanie_2/js/CustomTitle.js
@@ -3,23 +3,43 @@ import React, { useState, useEffect } from "react";
 const CustomTitle = () => {
     const [counter, setCounter] = useState(1);
 
-    const increment = () => {
-        setCounter((prevStaet) => prevStaet + 1);
+    const count = () => {
+        setCounter((prevState) => prevState + 1);
     };
 
     useEffect(() => {
-        document.title = `clicked: ${counter}`
-    })
-
-    
+        document.title = `clicked ${counter}`;
+    }, [counter]);
 
     return (
-        <p onClick={increment} style={{fontSize: counter * 1.2 + 'rem'}}>
+        <p onClick={count} style={{ fontSize: counter * 1.2 + 'rem' }}>
             kliknieto mnie juz {counter} razy
         </p>
     );
 };
 
-export default () => {
-    return <CustomTitle />;
-};
+export default CustomTitle;
+
+// import React, { useState, useEffect } from "react";
+
+// const CustomTitle = () => {
+//     const [counter, setCounter] = useState(1);
+
+//     const increment = () => {
+//         setCounter((prevStaet) => prevStaet + 1);
+//     };
+
+//     useEffect(() => {
+//         document.title = `clicked: ${counter}`
+//     })
+
+//     return (
+//         <p onClick={increment} style={{fontSize: counter * 1.2 + 'rem'}}>
+//             kliknieto mnie juz {counter} razy
+//         </p>
+//     );
+// };
+
+// export default () => {
+//     return <CustomTitle />;
+// };
