92a2e093263534e11e21423e0125353b6b526295
kamil079 <kamil.szczepaniec@wp.pl>
2022-06-20 19:51:34 +0200

    txt change ?:

--- 03_Dzien_2/03_Renderowanie_warunkowe/01_Zadanie_1/js/MyButton.js
+++ 03_Dzien_2/03_Renderowanie_warunkowe/01_Zadanie_1/js/MyButton.js
@@ -1,16 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-const MyButton = ({text}) => {
-    const [txt, setTxt] = useState(text)
+const MyButton = () => {
+    const [txt, setTxt] = useState('nie')
 
-    const toggle = () => {
-        setTxt(txt === "nie" ? 'tak' : 'nie')
-
+    const changeTxt = () => {
+        setTxt(txt == 'nie' ? 'tak' : 'nie')
     }
 
-    return <button onClick={toggle}>{txt}</button>
+    return <h1 onClick={changeTxt}>{txt}</h1>
 }
 
-export default () => {
-    return <MyButton text={'TAK'}/>
-}
+export default MyButton;
+
+// import React, {useState} from 'react';
+
+// const MyButton = ({text}) => {
+//     const [txt, setTxt] = useState(text)
+
+//     const toggle = () => {
+//         setTxt(txt === "nie" ? 'tak' : 'nie')
+
+//     }
+
+//     return <button onClick={toggle}>{txt}</button>
+// }
+
+// export default () => {
+//     return <MyButton text={'TAK'}/>
+// }
