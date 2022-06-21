caae665e94d4f6ea6bb23bc25cc51f287c131191
kamil079 <kamil.szczepaniec@wp.pl>
2022-06-21 19:56:52 +0200

    random nr if more/less

--- 03_Dzien_2/03_Renderowanie_warunkowe/02_Zadanie_2/js/NumberRandomInfo.js
+++ 03_Dzien_2/03_Renderowanie_warunkowe/02_Zadanie_2/js/NumberRandomInfo.js
@@ -1,24 +1,52 @@
 import React, { useEffect, useState } from "react";
 
 const NumberRandomInfo = () => {
-  const [nr, setNr] = useState(0);
+    const [nr, changeNr] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      let random = Math.ceil(Math.random() * 5);
-      setNr((prevState) => (prevState = random));
-    }, 1000);
+    useEffect(() => {
+        let intervalID = setInterval(() => {
+            changeNr((prevState) => (prevState = Math.ceil(Math.random() * 5)));
+        }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+        return () => {
+            clearInterval(intervalID);
+        };
+    }, [nr]);
 
-  return (
-    <h1>
-      {nr > 3 ? `nr ${nr} jest większy od 3` : `nr ${nr} jest mniejszy od 3`}
-    </h1>
-  );
+    let el;
+    if (nr > 3) {
+        el = `liczba ${nr} jest większa niż 3`;
+    } else if ((nr == 3)) {
+        el = `liczba ${nr} jest równa 3`;
+    } else {
+        el = `liczba ${nr} jest mniejsza niż 3`;
+    }
+
+    return <h1>{el}</h1>;
 };
+export default NumberRandomInfo;
 
-export default function () {
-  return <NumberRandomInfo />;
-}
+// import React, { useEffect, useState } from "react";
+
+// const NumberRandomInfo = () => {
+//   const [nr, setNr] = useState(0);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       let random = Math.ceil(Math.random() * 5);
+//       setNr((prevState) => (prevState = random));
+//     }, 1000);
+
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <h1>
+//       {nr > 3 ? `nr ${nr} jest większy od 3` : `nr ${nr} jest mniejszy od 3`}
+//     </h1>
+//   );
+// };
+
+// export default function () {
+//   return <NumberRandomInfo />;
+// }
