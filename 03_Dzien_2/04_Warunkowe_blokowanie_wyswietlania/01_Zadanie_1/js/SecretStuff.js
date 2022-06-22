7105b268e77670c0e922555c798f5ccea5f6823d
kamil079 <kamil.szczepaniec@wp.pl>
2022-06-22 20:02:49 +0200

    teneryoperator with props

--- 03_Dzien_2/04_Warunkowe_blokowanie_wyswietlania/01_Zadanie_1/js/SecretStuff.js
+++ 03_Dzien_2/04_Warunkowe_blokowanie_wyswietlania/01_Zadanie_1/js/SecretStuff.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from 'react'
 
-const SecretStuff = ({ password, correctPassword, secret }) => {
-  return <div>{password === correctPassword ? secret : null}</div>;
-};
+const SecretStuff = ({password, correctPassword, secret}) => {
 
-export default () => {
-  return (
-    <SecretStuff
-      password={"1234"}
-      correctPassword={"1234"}
-      secret={"superDuperTajneFajne"}
-    />
-  );
+
+  return <h1>{password === correctPassword ? secret : null}</h1>
+}
+
+export default function App() {
+  return <SecretStuff password={'pw'} correctPassword={'pw'} secret={'nobody knows'}/>
 };
+// import React from "react";
+
+// const SecretStuff = ({ password, correctPassword, secret }) => {
+//   return <div>{password === correctPassword ? secret : null}</div>;
+// };
+
+// export default () => {
+//   return (
+//     <SecretStuff
+//       password={"1234"}
+//       correctPassword={"1234"}
+//       secret={"superDuperTajneFajne"}
+//     />
+//   );
+// };
