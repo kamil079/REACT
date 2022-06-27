43bfe1995810fbd8a435fcdf0180978221d25f3b
kamil079 <kamil.szczepaniec@wp.pl>
2022-06-27 11:55:37 +0200

    dont get it

--- 04_Dzien_2_-_Praca_domowa/02_Zadanie_2/js/SpeedClickGame.js
+++ 04_Dzien_2_-_Praca_domowa/02_Zadanie_2/js/SpeedClickGame.js
@@ -1,37 +1,39 @@
 import React, { useEffect, useState } from "react";
 
 const SpeedClickGame = ({ timer }) => {
-  const [time, setTime] = useState(timer);
-  const [points, setPoints] = useState(0);
-
+    const [time, setTime] = useState(timer);
+    const [points, setPoints] = useState(0);
 
     const handleClick = () => {
-        setPoints(prevPoints => (prevPoints + 1))
-        setTime((prevState) => timer = prevState - 50)
-    }
+        setPoints((prevPoints) => prevPoints + 1);
+        setTime((prevState) => (timer = prevState - 50));
+    };
 
+    useEffect(() => {
+        let interval = setInterval(() => {
+            setTime((prevState) => prevState - 50);
+            // time <= 0 ? clearInterval(interval) : null;
 
-  useEffect(() => {
-    let interval = setInterval(() => {
-      setTime((prevState) => prevState - 50)
-    }, 1000);
+        }, 1000);
 
-    return () => clearInterval(interval)
-  }, []);
+        return () => clearInterval(interval);
+    }, []);
 
-  return (
-    <>
-      <button onClick={handleClick} disabled={time <= 0 ? true : false}>
-        Click Me!
-      </button>
-      <div>
-        <h1>{time}</h1>
-        <h2>{points} {points > 1 ? 'points' : 'point'}</h2>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <button onClick={handleClick} disabled={time <= 0 ? true : false}>
+                Click Me!
+            </button>
+            <div>
+                <h1>{time}</h1>
+                <h2>
+                    {points} {points > 1 ? "points" : "point"}
+                </h2>
+            </div>
+        </>
+    );
 };
 
 export default () => {
-  return <SpeedClickGame timer={50000} />;
+    return <SpeedClickGame timer={500} />;
 };
