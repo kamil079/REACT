a28fd74680d8b30c4162782149c5624391537129
kamil079 <kamil.szczepaniec@wp.pl>
2022-06-27 12:45:39 +0200

    light on/off

--- 05_Dzien_3/01_Przekazywanie_parametrow/01_Zadanie_1/js/Bulb.js
+++ 05_Dzien_3/01_Przekazywanie_parametrow/01_Zadanie_1/js/Bulb.js
@@ -1,21 +1,47 @@
 import React, { useState } from "react";
 
 const Bulb = () => {
-  const [bulb, setBulb] = useState("white");
-  const [txt, setTxt] = useState("ON");
+    const [light, setLight] = useState("yellow");
 
-  let handleChange = () => {
-    setBulb(bulb === "white" ? "yellow" : "white");
-    setTxt(txt === "OFF" ? "ONN" : "OFF");
-  };
+    const lightSwitch = () => {
+        setLight(light == "yellow" ? "white" : "yellow");
+    };
 
-  return (
-    <div style={{ height: "200px", width: "200px", background: bulb }}>
-      <button onClick={handleChange}>{txt}</button>
-    </div>
-  );
+    return (
+        <>
+            <div
+                style={{
+                    background: light,
+                    height: "100px",
+                    width: "100px",
+                }}
+            >
+                <button onClick={() => lightSwitch()}>{light == 'yellow' ? 'off' : 'on'}</button>
+            </div>
+        </>
+    );
 };
 
-export default () => {
-  return <Bulb />;
-};
+export default Bulb;
+
+// import React, { useState } from "react";
+
+// const Bulb = () => {
+//   const [bulb, setBulb] = useState("white");
+//   const [txt, setTxt] = useState("ON");
+
+//   let handleChange = () => {
+//     setBulb(bulb === "white" ? "yellow" : "white");
+//     setTxt(txt === "OFF" ? "ONN" : "OFF");
+//   };
+
+//   return (
+//     <div style={{ height: "200px", width: "200px", background: bulb }}>
+//       <button onClick={handleChange}>{txt}</button>
+//     </div>
+//   );
+// };
+
+// export default () => {
+//   return <Bulb />;
+// };
