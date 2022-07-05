68d08f62b8d7656a19473f23757cbf0d11effcb7
kamil079 <kamil.szczepaniec@wp.pl>
2022-07-05 14:32:33 +0200

    trainee

--- 05_Dzien_3/02_Przekazywanie_zdarzen/02_Zadanie_2/js/Shop.js
+++ 05_Dzien_3/02_Przekazywanie_zdarzen/02_Zadanie_2/js/Shop.js
@@ -5,24 +5,51 @@ const Shop = () => {
     const [list, setList] = useState([]);
 
     const onBuy = (title) => {
-        console.log(title)
         setList([...list, title]);
     };
 
     return (
         <>
-            <div>
-                <ShopItem onBuy={onBuy} title="MacBook Pro" />
-                <ShopItem onBuy={onBuy} title="Dell X5500" />
-                <ShopItem onBuy={onBuy} title="Asus NT6000" />
-                <ul>
-                    {list.map((el, i) => {
-                        return <li key={i}>{el}</li>;
-                    })}
-                </ul>
-            </div>
+            <ShopItem onClick={onBuy} title="myszka" />
+            <ShopItem onClick={onBuy} title="monitor" />
+            <ShopItem onClick={onBuy} title="komputer" />
+            <ShopItem onClick={onBuy} title="klawiatura" />
+            <ShopItem onClick={onBuy} title="laptop" />
+            <ul>
+                {list.map((el, i) => {
+                    return <li key={i}>{el}</li>;
+                })}
+            </ul>
         </>
     );
 };
 
 export default Shop;
+// import React, { useState } from "react";
+// import ShopItem from "./ShopItem";
+
+// const Shop = () => {
+//     const [list, setList] = useState([]);
+
+//     const onBuy = (title) => {
+//         console.log(title)
+//         setList([...list, title]);
+//     };
+
+//     return (
+//         <>
+//             <div>
+//                 <ShopItem onBuy={onBuy} title="MacBook Pro" />
+//                 <ShopItem onBuy={onBuy} title="Dell X5500" />
+//                 <ShopItem onBuy={onBuy} title="Asus NT6000" />
+//                 <ul>
+//                     {list.map((el, i) => {
+//                         return <li key={i}>{el}</li>;
+//                     })}
+//                 </ul>
+//             </div>
+//         </>
+//     );
+// };
+
+// export default Shop;
--- 05_Dzien_3/02_Przekazywanie_zdarzen/02_Zadanie_2/js/ShopItem.js
+++ 05_Dzien_3/02_Przekazywanie_zdarzen/02_Zadanie_2/js/ShopItem.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ShopItem = ({ title, onBuy }) => {
+const ShopItem = ({ title, onClick }) => {
     const handleClick = () => {
-        if (typeof onBuy === "function") {
-            onBuy(title);
+        if (typeof onClick === "function") {
+            onClick(title);
         }
     };
 
+
+
     return (
         <>
-            <div>
-                <h1>{title}</h1>
-                <button onClick={handleClick}>Kup</button>
-            </div>
+            <h1>{title}</h1>
+            <button onClick={handleClick}>get!</button>
         </>
     );
 };
 
 export default ShopItem;
+
+// import React from "react";
+
+// const ShopItem = ({ title, onBuy }) => {
+//     const handleClick = () => {
+//         if (typeof onBuy === "function") {
+//             onBuy(title);
+//         }
+//     };
+
+//     return (
+//         <>
+//             <div>
+//                 <h1>{title}</h1>
+//                 <button onClick={handleClick}>Kup</button>
+//             </div>
+//         </>
+//     );
+// };
+
+// export default ShopItem;
