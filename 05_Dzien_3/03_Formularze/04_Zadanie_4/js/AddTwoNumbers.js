f36d4277680b618a730d89bd3c890d080c243d55
kamil079 <kamil.szczepaniec@wp.pl>
2022-07-13 20:12:30 +0200

    form add two nrs

--- 05_Dzien_3/03_Formularze/04_Zadanie_4/js/AddTwoNumbers.js
+++ 05_Dzien_3/03_Formularze/04_Zadanie_4/js/AddTwoNumbers.js
@@ -1,29 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const AddTwoNumbers = () => {   
-    const [form, setForm] = useState({nr1: 0, nr2: 0})
+const AddTwoNumbers = () => {
+    const [form, setForm] = useState({ nr1: 0, nr2: 0 });
 
     const handleSubmit = (e) => {
-        const {name, value} = e.target
+        const { name, value } = e.target;
         e.preventDefault();
-        setForm(prevState => {
+        setForm((prevState) => {
             return {
                 ...prevState,
                 [name]: value,
-            }
-        })
-    }
-
+            };
+        });
+    };
 
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <input type='number' name='nr1' value={form.nr1} onChange={handleSubmit}/>
-                <input type='number' name='nr2' value={form.nr2} onChange={handleSubmit}/>
-                <h1 onChange={handleSubmit}>{parseInt(form.nr1) + parseInt(form.nr2)} </h1>
+                <input
+                    type="text"
+                    name="nr1"
+                    value={form.nr1}
+                    onChange={handleSubmit}
+                />
+                <input
+                    type="text"
+                    name="nr2"
+                    value={form.nr2}
+                    onChange={handleSubmit}
+                />
+                <h1 onChange={handleSubmit}>
+                    {form.nr1 || form.nr2
+                        ? parseInt(form.nr1) + parseInt(form.nr2)
+                        : "podaj dwie liczby"}{" "}
+                </h1>
             </form>
         </>
-    )
-}
+    );
+};
 
 export default AddTwoNumbers;
