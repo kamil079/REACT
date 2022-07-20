c9fd71ebabc3ab25a59f9511b80e0ad32bbb50d5
kamil079 <kamil.szczepaniec@wp.pl>
2022-07-20 12:39:35 +0200

    creat as many li as value of input

---
+++ 06_Dzien_3_-_Praca_domowa/03_Zadanie_3/js/DynamicLego.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+const DynamicLego = () => {
+    const [lego, setLego] = useState("");
+
+    let arr = [];
+    for (let i = 1; i <= lego; i++) {
+        arr.push(i);
+    }
+
+    return (
+        <form>
+            <input
+                type="number"
+                name="lego"
+                value={lego}
+                onChange={(e) => setLego(e.target.value)}
+            />
+            <ul>
+                {arr.map((el, i) => {
+                    return <li key={i}>{el}</li>;
+                })}
+            </ul>
+        </form>
+    );
+};
+
+export default DynamicLego;
